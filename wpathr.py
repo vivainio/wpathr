270c78f9a74efdfcbda4d3f1bbd846ab72e51d3a
Ville M. Vainio <ville.vainio@basware.com>
2014-11-20 22:47:42 +0200

    use winreg for get/set env

--- wpathr.py
+++ wpathr.py
@@ -1,7 +1,47 @@
 import os
-
 import ctypes
 from ctypes import wintypes
+
+import _winreg
+
+import sys
+from subprocess import check_call
+if sys.hexversion > 0x03000000:
+    import winreg
+else:
+    import _winreg as winreg
+
+class Win32Environment:
+    """Utility class to get/set windows environment variable"""
+
+    def __init__(self, scope):
+        assert scope in ('user', 'system')
+        self.scope = scope
+        if scope == 'user':
+            self.root = winreg.HKEY_CURRENT_USER
+            self.subkey = 'Environment'
+        else:
+            self.root = winreg.HKEY_LOCAL_MACHINE
+            self.subkey = r'SYSTEM\CurrentControlSet\Control\Session Manager\Environment'
+
+    def getenv(self, name):
+        key = winreg.OpenKey(self.root, self.subkey, 0, winreg.KEY_READ)
+        try:
+            value, _ = winreg.QueryValueEx(key, name)
+        except WindowsError:
+            value = ''
+        return value
+
+    def setenv(self, name, value):
+        # Note: for 'system' scope, you must run this as Administrator
+        key = winreg.OpenKey(self.root, self.subkey, 0, winreg.KEY_ALL_ACCESS)
+        winreg.SetValueEx(key, name, 0, winreg.REG_EXPAND_SZ, value)
+        winreg.CloseKey(key)
+        # For some strange reason, calling SendMessage from the current process
+        # doesn't propagate environment changes at all.
+        # TODO: handle CalledProcessError (for assert)
+
+
 _GetShortPathNameW = ctypes.windll.kernel32.GetShortPathNameW
 _GetShortPathNameW.argtypes = [wintypes.LPCWSTR, wintypes.LPWSTR, wintypes.DWORD]
 _GetShortPathNameW.restype = wintypes.DWORD
@@ -19,7 +59,12 @@ def get_short_path_name(long_name):
             return output_buf.value
         else:
             output_buf_size = needed
+
 oldpath = os.environ["PATH"]
+
+eu = Win32Environment(scope = 'user')
+
+oldpath = eu.getenv("PATH")
 
 paths = oldpath.split(";")
 newpath =  ";".join([get_short_path_name(p) for p in paths])
