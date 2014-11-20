6fd35c022060a440c829fad1089d0d15a906e375
Ville M. Vainio <ville.vainio@basware.com>
2014-11-20 23:17:56 +0200

    more fooling around

---
+++ args.py
@@ -0,0 +1,41 @@
+""" Quick and dirty wrapper for argparse, with the intent to be less verbose
+
+Meant to be used like this::
+
+    import args
+    s1 = args.sub("check", do_check, ["filename"])
+    s1 = args.sub("sign", do_sign, ["filename"])
+
+
+    ops = args.parse()
+
+"""
+
+import argparse, sys
+
+p = argparse.ArgumentParser()
+
+arg = p.add_argument
+
+subparsers = p.add_subparsers()
+
+def parse():
+    """ Call this after declaring your arguments
+    """
+    parsed = p.parse_args(sys.argv[1:])
+    if parsed.func:
+        parsed.func(parsed)
+    return parsed
+
+def sub(name, func, arg = None):
+    """ Add subparser
+
+    """
+    sp = subparsers.add_parser(name)
+    sp.set_defaults(func=func)
+    if arg is not None:
+        for a in arg:
+            sp.add_argument(a, type=str)
+    return sp
+
+
