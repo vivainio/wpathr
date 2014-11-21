fbf7a2acf09f5b70a4b88193fc9aa5df6c94228d
Ville M. Vainio <ville.vainio@basware.com>
2014-11-21 20:44:54 +0200

    packaging start

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(name='wpathr',
+      version='0.1',
+      description='Path optimization tool for Windows',
+      author='Ville Vainio',
+      author_email='vivainio@gmail.com',
+      url='https://github.com/vivainio/wpathr',
+      packages=['wpathr'],
+      entry_points = {
+        'console_scripts': [
+            'wpathr = wpathr.main',
+        ]
+      }
+     )
