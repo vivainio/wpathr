240080533e937b0ec129947532d0d7c542646ffa
Ville M. Vainio <ville.vainio@basware.com>
2014-12-13 15:40:08 +0200

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='wpathr',
-      version='0.1.4',
+      version='0.1.5',
       description='Path optimization tool for Windows',
       author='Ville Vainio',
       author_email='vivainio@gmail.com',
