d5075ab2acdf6bfe5074d723205639b886d622ca
Ville M. Vainio <ville.vainio@basware.com>
2019-05-16 21:18:30 +0300

    python 3 port

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='wpathr',
-      version='0.3.1',
+      version='0.4.0',
       description='Path optimization tool and command runner for Windows',
       author='Ville Vainio',
       author_email='vivainio@gmail.com',
